dfc9e6a6a68dbf2c5063d491b4796bed6afee089
zahid-official <zahid.official8@gmail.com>
2025-12-08 02:08:40 +0600

    Fix bugs in dropdown mene ui component

---
+++ src/app/(dashboardLayout)/layout.tsx
@@ -0,0 +1,10 @@
+// DashboardLayout Component
+const DashboardLayout = () => {
+  return (
+    <div>
+      <h1>Welcome to the DashboardLayout Component</h1>
+    </div>
+  );
+};
+
+export default DashboardLayout;
