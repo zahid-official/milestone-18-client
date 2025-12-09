716b0fa65d23d6c0f040e15511a9f17becbe8c1b
zahid-official <zahid.official8@gmail.com>
2025-12-09 20:09:58 +0600

    Create reusable table header

--- src/app/(dashboardLayout)/(common)/change-password/page.tsx
+++ src/app/(dashboardLayout)/(common)/change-password/page.tsx
@@ -1,8 +1,15 @@
+import ManagementTableHeader from "@/components/modules/dashboard/table/ManagementTableHeader";
+
 // ChangePasswordPage Component
 const ChangePasswordPage = () => {
   return (
     <div>
       <h1>Welcome to the ChangePasswordPage Component</h1>
+      <ManagementTableHeader
+        title="Hello"
+        description="Bellow"
+        action={{ label: "Add Vendor" }}
+      />
     </div>
   );
 };
