68e42c5a5dcd513f3b4a36f23b998012f82123ea
zahid-official <zahid.official8@gmail.com>
2025-12-12 18:32:32 +0600

    Product details for shop page

---
+++ src/app/(commonLayout)/shop/[productId]/page.tsx
@@ -0,0 +1,12 @@
+import ProductDetails from "@/components/modules/common/shop/ProductDetails";
+
+// page Component
+const page = () => {
+  return (
+    <div>
+      <ProductDetails />
+    </div>
+  );
+};
+
+export default page;
--- src/components/modules/common/shop/ShopProduct.tsx
+++ src/components/modules/common/shop/ShopProduct.tsx
@@ -1,8 +1,9 @@
 import collection from "@/assets/collection-1.jpg";
 import PageBanner from "@/components/shared/PageBanner";
 import { Button } from "@/components/ui/button";
-import { ShoppingBasket } from "lucide-react";
+import { Eye } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 
 // ShopProduct Component
 const ShopProduct = () => {
@@ -22,9 +23,7 @@ const ShopProduct = () => {
           <h2 className="sm:text-3xl text-2xl font-semibold font-heading">
             Browse Collections
           </h2>
-          <p className="text-foreground/60">
-            Best Seller and Trending Product
-          </p>
+          <p className="text-foreground/60">Best Seller and Trending Product</p>
         </div>
 
         <div className="lg:grid grid-cols-3 flex flex-wrap justify-center items-center gap-10">
@@ -42,10 +41,12 @@ const ShopProduct = () => {
                 <div className="absolute inset-0 bg-black/0 group-hover:bg-black/10 transition-all duration-300"></div>
 
                 {/* Sliding Button */}
-                <Button className="absolute left-1/2 -translate-x-1/2 group-hover:-translate-y-16 opacity-0 group-hover:opacity-100 transition-all duration-300">
-                  <ShoppingBasket />
-                  Add to Cart
-                </Button>
+                <Link href={`/shop/${"12233555"}`}>
+                  <Button className="absolute left-1/2 -translate-x-1/2 group-hover:-translate-y-16 opacity-0 group-hover:opacity-100 transition-all duration-300">
+                    <Eye />
+                    View Details
+                  </Button>
+                </Link>
               </div>
 
               {/* Text */}
