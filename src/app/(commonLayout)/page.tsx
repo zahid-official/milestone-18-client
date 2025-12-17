690de88dc2138cee31c61796af89b6fd755f3a50
zahid-official <zahid.official8@gmail.com>
2025-12-17 18:56:26 +0600

    Update home page's collection section

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -2,7 +2,6 @@ import Banner from "@/components/modules/common/home/Banner";
 import Category from "@/components/modules/common/home/Category";
 import Collection from "@/components/modules/common/home/Collection";
 import Feature from "@/components/modules/common/home/Feature";
-import Newsletter from "@/components/modules/common/home/Newsletter";
 import Testimonial from "@/components/modules/common/home/Testimonial";
 
 // HomePage Component
@@ -14,7 +13,6 @@ const HomePage = () => {
       <Category />
       <Collection />
       <Testimonial />
-      <Newsletter />
     </div>
   );
 };
